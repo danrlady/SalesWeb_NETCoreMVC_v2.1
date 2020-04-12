f5c40e99e8436b614e4164d3fe3947329ff78f4e
Danrley <danrleybruno@gmail.com>
2020-04-12 19:04:52 -0300

    Second migration - OtherEntities

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -9,6 +9,8 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
