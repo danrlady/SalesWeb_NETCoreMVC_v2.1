3d9930a71faa40c339a30322e6cf63a7f42c7661
Danrley <danrleybruno@gmail.com>
2020-04-12 21:33:25 -0300

    Referential integrity for sellers

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
@@ -23,6 +19,7 @@ namespace SalesWebMVC.Controllers
             return View(list);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
